5121212a698cdbc2e98e3f514f4298fe18c7ea77
Pablo <84303588+Puetate@users.noreply.github.com>
2023-06-22 14:35:28 -0500

    Enable Cors

--- Backend/src/main/java/com/capibaracode/backend/BackendApplication.java
+++ Backend/src/main/java/com/capibaracode/backend/BackendApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @SecurityScheme(name = "swagger",scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
@@ -13,5 +16,7 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
+
+
 
 }
